a3cc4ff406397e306f1681b59c11cea5d0e0cb1c
dozinq <dojinuk@kakao.com>
2022-09-25 16:19:59 +0900

    [feature] complete search function making

---
+++ frontend/src/components/search/Element.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export default function Element(props) {
+  return (
+    <StyledWrapper>
+      <div id="picture">
+        <img src={props.img_link} alt="example" />
+      </div>
+      <div id="information">
+        <div>{props.name}</div>
+        <div>{props.brewery}</div>
+        <div>{props.ingredients}</div>
+        <div>
+          {props.size}ml / {props.alcohol}도
+        </div>
+        <div id="information-desc">{props.desc}</div>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  display: flex;
+  border: solid;
+  margin: 10px 0px;
+
+  #picture {
+    width: 200px;
+    height: 200px;
+    border: solid;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #picture img {
+    width: 190px;
+    height: 190px;
+    margin: auto;
+    object-fit: contain;
+  }
+  #information {
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+  }
+  #information div {
+    margin: 0px 5px;
+  }
+  #information-desc {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    line-height: 1.2em;
+  }
+`;
--- frontend/src/views/search/SearchPage.js
+++ frontend/src/views/search/SearchPage.js
@@ -1,16 +1,29 @@
 import Header from "components/nav/Header";
 import styled from "styled-components";
-import SearchFrame from "components/search/SearchFrame";
+import SearchBar from "components/search/SearchBar";
+import SearchFilter from "components/search/SearchFilter";
 import SearchResult from "components/search/SearchResult";
+import * as React from "react";
 
 export default function SearchPage() {
-  return(
-    <StyledWrapper>
-      <Header />
-      <SearchFrame />
-      <SearchResult />
-    </StyledWrapper>
-  );
-};
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [filterKinds, setFilterKinds] = React.useState([]);
 
-const StyledWrapper = styled.div``;
+  return (
+    <>
+      <Header />
+      <StyledWrapper>
+        <SearchBar setSearchQuery={setSearchQuery} />
+        <SearchFilter filterKinds={ filterKinds } setFilterKinds={setFilterKinds} />
+        <SearchResult searchQuery={searchQuery} filterKinds={filterKinds} />
+      </StyledWrapper>
+    </>
+  );
+}
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
