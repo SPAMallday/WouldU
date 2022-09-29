8389558fad3944a5f8b0f20d1db01f8b0b64c56f
한재승 <gkswotmd96@naver.com>
2022-09-30 01:03:20 +0900

    [refactor] login 수정

---
+++ frontend/src/components/mypage/MyCollection.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import earth from "assets/img/earth.png";
+import planet1 from "assets/img/planet1.png";
+import planet2 from "assets/img/planet2.png";
+import planet3 from "assets/img/planet3.png";
+import space from "assets/img/space_example.jpg";
+
+export default function MyCollection(prop) {
+  const [rank, setRank] = useState();
+  useEffect(() => {
+    console.log(prop);
+    console.log(prop.spaceData);
+
+    if (prop.spaceData && prop.spaceData.length > 0) {
+      setRank(prop.spaceData);
+      console.log(rank);
+    }
+  }, [prop, rank]);
+
+  if (prop.spaceData && prop.spaceData.length > 0) {
+    if (rank && rank.length > 0) {
+      return (
+        <StyledWrapper>
+          <div id="main">
+            <h3 id="title">{prop.userName}님이 먹은 술 </h3>
+            <div id="space">
+              {rank[0].count !== 0 ? (
+                <Earth count={rank[0].count}>
+                  <div id="textCount">{rank[0].count} 개</div>
+                  <img src={earth} alt="지구" id="P_earth" />
+                  <h5>{rank[0].space}</h5>
+                </Earth>
+              ) : (
+                <Earth count={7}>
+                  <img src={earth} alt="지구" id="P_earth" />
+                  <h5>지구</h5>
+                </Earth>
+              )}
+              {rank[1] ? (
+                <Planet1 count={rank[1].count}>
+                  <div id="textCount">{rank[1].count} 개</div>
+                  <img src={planet1} alt="행성" id="P_planet1" />
+                  <h5>{rank[1].space}</h5>
+                </Planet1>
+              ) : null}
+              {rank[2] ? (
+                <Planet2 count={rank[2].count}>
+                  <div id="textCount">{rank[2].count} 개</div>
+                  <img src={planet2} alt="행성" id="P_planet2" />
+                  <h5>{rank[2].space}</h5>
+                </Planet2>
+              ) : null}
+
+              {rank[3] ? (
+                <Planet3 count={rank[3].count}>
+                  <div id="textCount">{rank[3].count} 개</div>
+                  <img src={planet3} alt="행성" id="P_planet3" />
+                  <h5>{rank[3].space}</h5>
+                </Planet3>
+              ) : null}
+            </div>
+          </div>
+        </StyledWrapper>
+      );
+    }
+  }
+}
+
+const Earth = styled.div`
+  #P_earth {
+    width: ${props =>
+      props.count > 3 ? (props.count > 6 ? "300px" : "200px") : "100px"};
+  }
+
+  display: inline-block;
+  color: white;
+  margin-left: 10px;
+  margin-right: 10px;
+
+  h5 {
+    text-align: center;
+    margin-top: 10px;
+  }
+
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+
+const Planet1 = styled.div`
+  #P_planet1 {
+    width: ${props =>
+      props.count > 3 ? (props.count > 6 ? "300px" : "200px") : "100px"};
+  }
+
+  display: inline-block;
+  color: white;
+  h5 {
+    text-align: center;
+    margin-top: 10px;
+  }
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+const Planet2 = styled.div`
+  #P_planet2 {
+    width: ${props =>
+      props.count > 3 ? (props.count > 6 ? "300px" : "200px") : "100px"};
+  }
+
+  display: inline-block;
+  color: white;
+  h5 {
+    text-align: center;
+    margin-top: 10px;
+  }
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+const Planet3 = styled.div`
+  #P_planet3 {
+    width: ${props =>
+      props.count > 3 ? (props.count > 6 ? "300px" : "200px") : "100px"};
+  }
+
+  display: inline-block;
+  color: white;
+  h5 {
+    text-align: center;
+    margin-top: 10px;
+  }
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+
+const StyledWrapper = styled.div`
+  #main {
+    margin-top: 60px;
+    margin-bottom: 40px;
+    background: url("${space}");
+    background-size: 100% 100%;
+    width: 1300px;
+    height: 510px;
+  }
+  #title {
+    text-align: left;
+    padding-top: 20px;
+    margin-left: 30px;
+    color: white;
+    margin-bottom: 30px;
+  }
+
+  #space {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 400px;
+  }
+`;
---
+++ frontend/src/views/login/Login.js
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Header from "components/nav/Header";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import swal from "sweetalert";
+import logo2 from "assets/img/logo2.png";
+import Card from "@mui/material/Card";
+import space from "assets/img/space_example.jpg";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+
+  const handleInputId = e => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = e => {
+    setInputPw(e.target.value);
+  };
+
+  async function onClickLogin(event) {
+    if (inputId !== "") {
+      if (inputPw !== "") {
+        const data = {
+          user_id: inputId,
+          password: inputPw,
+        };
+        await axios
+          .post("http://localhost:8000/user/signin", data)
+          .then(res => {
+            console.log(res);
+            if (res.data.result === "success") {
+              event.preventDefault();
+              sessionStorage.setItem("ID", inputId);
+              sessionStorage.setItem("no", res.data.user_no);
+              sessionStorage.setItem("Nick", res.data.nickname);
+              navigate("/");
+            } else {
+              swal("Error!", "아이디 또는 비밀번호 오류입니다.", "error");
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        swal("Error!", "비밀번호를 입력해주세요!!", "error");
+      }
+    } else {
+      swal("Error!", "아이디를 입력해주세요!!", "error");
+    }
+
+    console.log("ID", inputId);
+    console.log("PW", inputPw);
+  }
+
+  return (
+    <StyledWrapper>
+      <Header />
+      <div id="main">
+        <Card variant="outlined" sx={{ width: 1300, mt: 10 }}>
+          <h2>Login</h2>
+          <img src={logo2} alt="logo"></img>
+          <div id="loginForm">
+            <div id="loginBox">
+              <div id="inputH">
+                <div>
+                  <TextField
+                    label="아이디"
+                    variant="outlined"
+                    size="small"
+                    value={inputId}
+                    onChange={handleInputId}
+                    id="input_area"
+                  />
+                </div>
+              </div>
+
+              <div id="inputBox">
+                <div>
+                  <TextField
+                    autoComplete="current-password"
+                    label="비밀번호"
+                    type="password"
+                    variant="outlined"
+                    value={inputPw}
+                    onChange={handleInputPw}
+                    size="small"
+                    id="input_area"
+                  />
+                </div>
+              </div>
+
+              <div id="btBox">
+                <Button
+                  id="btn_login"
+                  color="secondary"
+                  variant="contained"
+                  onClick={onClickLogin}
+                >
+                  Login
+                </Button>
+              </div>
+              <Link to="/join">
+                <div id="textJoin">
+                  <h5 id="h_join">회원가입</h5>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  #main {
+    height: 900px;
+    text-align: center;
+    background: url("${space}");
+    background-size: 100% 100%;
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+  }
+
+  .css-rb59ar-MuiPaper-root-MuiCard-root {
+    margin: 90px auto;
+    border: 5px solid;
+  }
+
+  #main img {
+    margin-top: -330px;
+  }
+
+  #main h2 {
+    margin-top: 50px;
+  }
+
+  #loginBox {
+    margin-right: -100px;
+    margin-bottom: 100px;
+    width: 600px;
+    display: inline-block;
+  }
+
+  #input_area {
+    width: 250px;
+  }
+
+  #inputH {
+    margin-top: 70px;
+  }
+
+  #inputBox {
+    width: 300px;
+    margin: 50px auto;
+  }
+
+  #btBox {
+    margin: 70px;
+  }
+
+  #btn_login {
+    margin-top: 20px;
+    width: 280px;
+  }
+
+  #loginForm {
+    display: inline-block;
+    margin-top: 50px;
+  }
+
+  #textJoin {
+    width: 100px;
+    margin: auto;
+  }
+
+  #h_join {
+    color: purple;
+  }
+`;
