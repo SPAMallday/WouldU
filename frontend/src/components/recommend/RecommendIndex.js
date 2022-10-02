5742b7f8d889385feb6a78467188de574b8515b1
한재승 <gkswotmd96@naver.com>
2022-10-03 02:58:30 +0900

    [refactor] 페이지 권한설정

--- frontend/src/components/recommend/RecommendIndex.js
+++ frontend/src/components/recommend/RecommendIndex.js
@@ -1,28 +1,44 @@
 import styled from "styled-components";
-import * as React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import earth from "assets/img/earth.png";
-import planet1 from "assets/img/planet1.png";
+import planet1 from "assets/img/planet3.png";
 import planet2 from "assets/img/planet2.png";
 
 /**
  * 리뷰를 한번도 하지 않은 유저에게는 '평가'만 활성화하여 보여지고, 안내 문구도 같이 출력해야 함
  */
 export default function RecommendIndex() {
+  const navigate = useNavigate();
+  const [user_no, setno] = useState();
+  useEffect(() => {
+    setno(window.sessionStorage.getItem("no"));
+  }, []);
+
+  const sfr = () => {
+    if (user_no === null) {
+      swal("앗!", "로그인이 필요한 서비스입니다.", "info");
+    } else {
+      navigate("/recommend/search-for-recommend");
+    }
+  };
+  const boe = () => {
+    if (user_no === null) {
+      swal("앗!", "로그인이 필요한 서비스입니다.", "info");
+    } else {
+      navigate("/recommend/based-on-evaluation");
+    }
+  };
   return (
     <StyledWrapper>
-      <div>
-        <Link to="/recommend/based-on-evaluation">
-          <img src={planet1} alt="행성" />
-          <div id="text">술 평가 기반 추천 받으러 가기</div>
-        </Link>
+      <div onClick={boe}>
+        <img src={planet1} alt="행성" />
+        <div id="text">술 평가 기반 추천 받으러 가기</div>
       </div>
-      <div>
-        <Link to="/recommend/search-for-recommend">
-          <img src={earth} alt="지구" />
-          <div id="text">술 평가하러 가기</div>
-        </Link>
+      <div onClick={sfr}>
+        <img src={earth} alt="지구" />
+        <div id="text">술 평가하러 가기</div>
       </div>
       <div>
         <Link to="/recommend/based-on-filtering">
