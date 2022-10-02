6ce08874dc6fe914b4d67b108ee0b0e9c07c4682
hyejin <hyejin23@gmail.com>
2022-10-03 06:13:43 +0900

    [fix] Resolve merge conflict

--- backend/alcohol/urls.py
+++ backend/alcohol/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import alcoDetails, alcoIsLike , alcoPostReview, alcoReviewAPI, similarAlcoholAPI
-=======
-from .views import alcoDetails, alcoIsLike , alcoPostReview, RankByUserKind
->>>>>>> 7f45649de5ff53c0512220ec1a4ec1fa3a37d90e
+from .views import alcoDetails, alcoIsLike , alcoPostReview, RankByUserKind, alcoReviewAPI, similarAlcoholAPI
 
 urlpatterns = [
     path('detail/<alco_no>/<user_no>', alcoDetails),
     path('like', alcoIsLike),
     path('review', alcoPostReview),
-<<<<<<< HEAD
     
-    path('detail/<alcohol_no>', alcoReviewAPI),
-    path('similar-alcohol/<int:alcohol_no>', similarAlcoholAPI)
-=======
+    path('review/<alcohol_no>', alcoReviewAPI),
+    path('similar-alcohol/<int:alcohol_no>', similarAlcoholAPI),
     path('rank-type/<user_no>', RankByUserKind)
->>>>>>> 7f45649de5ff53c0512220ec1a4ec1fa3a37d90e
 ]
