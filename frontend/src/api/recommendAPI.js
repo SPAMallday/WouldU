5b9c14a0d263f2873dd328404667c5490d5a4f5e
최지혜 <chlwlgp11@naver.com>
2022-10-03 18:15:16 +0900

    Update recommendAPI.js

--- frontend/src/api/recommendAPI.js
+++ frontend/src/api/recommendAPI.js
@@ -91,7 +91,7 @@ export const getRecord = async data => {
   }
   try {
     const res = await apiClient.get(`/recommend/record`, {
-      // params: { user_no: user_no, alcohol_no: data },
+      params: { user_no: user_no, alcohol_no: data },
     });
     return res.data;
   } catch (err) {
@@ -107,7 +107,7 @@ export const makeRecord = async data => {
   }
   try {
     const res = await apiClient.post(`/recommend/record/update`, {
-      // user_no: user_no,
+      user_no: user_no,
       ...data,
     });
     return res;
