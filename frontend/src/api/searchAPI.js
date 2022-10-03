f36c93f1cd11c10a4c44a547d97d2766f155061a
최지혜 <chlwlgp11@naver.com>
2022-10-03 21:04:40 +0900

    Update searchAPI.js

--- frontend/src/api/searchAPI.js
+++ frontend/src/api/searchAPI.js
@@ -4,8 +4,8 @@ import { apiClient } from ".";
 // 검색
 export const search = async (params) => {
   try {
-    const res = await apiClient.get(`/search`, {params : params});
-    //const res = await apiClient.get(`/search?name=${params.name}&alcol_type=${params.alcol_type}&row_index=${params.row_index}&sort=${params.sort}`);
+    //const res = await apiClient.get(`/search`, {params : params});
+    const res = await apiClient.get(`/search?name=${params.name}&alcol_type=${params.alcol_type}&row_index=${params.row_index}&sort=${params.sort}`);
     return res.data;
   } catch (err) {
     console.log(err);
